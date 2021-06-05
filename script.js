10948601f6b2621de4fc7947c282c05e2df6043c
Vadim Semchanka <vadimsemch@gmail.com>
2021-06-05 17:52:39 +0200

    lesson_06 complete

--- script.js
+++ script.js
@@ -1,91 +1,44 @@
 'use strict'
 
+
 let isNumber = function(n) {
      return !isNaN(parseFloat(n)) && isFinite(n)
 };
 
-let money,
-    income = 'услуги', 
-    addExpenses = 'квартира, коммунальные, интернет',
-    mission = 10000,
-    period = 6,
-    amount,
-    expenses1,
-    expenses2;
+const numberX = Math.floor(100*Math.random())+1;
+console.log('Число бота: ' + numberX);
+const gess = +prompt('Угадай число от 1 до 100');
+console.log(gess); 
 
-//     1) Переписать функцию start циклом do while
-const start = function() {
-     let n = 0;
-     do {
-          money = prompt('Ваш месячный доход?');
-          n++;
-     } 
-     while(!isNumber(money)); 
-}
-start();
+const getGessNumber = function() {
+     if (gess > numberX ) {
+          let j = 0;
+          function data(){
+               j++;
+               if (j === 1) {
+                    return ('Загаданное число меньше');
+               } 
+               data();
+          }
+          data();
+          
+              
+     } else if (gess < numberX) {
+          return ('Загаданное число больше');
 
-
-addExpenses =  prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log(addExpenses.split(', '));
-
-let deposit = confirm('Есть ли у вас депозит в банке?');
-console.log(deposit);
-
-
-// 2) Добавить проверку что введённые данные являются числом,
-//  которые мы получаем на вопрос 'Во сколько это обойдется?’
-//   в функции  getExpensesMonth
-
-function getExpensesMonth (){
-     let sum = 0;
-     for(let i = 0; i < 2; i++){
-             if (i==0) expenses1 = prompt('Какие обязательные расходы вы планируете в месяце?');
-             else expenses2 = prompt('Какие еще расходы вы планируете в месяце?');
-         do amount = +prompt('Во сколько это обойдется?');
-         while(!isNumber(amount)) sum += amount;
+             
+     } else if (!isNumber(gess)) {
+          return ('Введи число!') ; 
+          
+     } else if (gess === null) {
+          return ('Игра окончена');
+     } else if (gess === numberX) {
+          return ('Поздравляю, Вы угадали!!!');
      }
-     return sum;
 };
+console.log(getGessNumber());
 
 
-const accum = getExpensesMonth();
-const getAccumulatedMonth = function(sum,ras) {
-     return sum - ras;
-};
-console.log('Накопления за месяц  ' + (getAccumulatedMonth(money, accum)));
 
 
-const accumulatedMonth = getAccumulatedMonth(money, accum);
 
-
-// 3) Если getTargetMonth возвращает нам отрицательное значение, то вместо
-//  “Цель будет достигнута” необходимо выводить “Цель не будет достигнута”
-
-const getTargetMonth = function(summ, acc) {
-          return summ / acc;
- 
-};
-if (getTargetMonth(mission, accumulatedMonth) >= 0) {
-     console.log ('достигнута цель через ' + Math.floor(getTargetMonth(mission, accumulatedMonth)) + ' месяца');
-} else {
-     console.log ('Цель не будет достигнута');
-}   
-
-const budgetDay = accumulatedMonth / 30;
-console.log('Цель будет достигнута за ' + Math.ceil(budgetDay) + ' месяца');
-
-
-const getStatusIncome = function() {
-     if (budgetDay > 1200) {
-          return('У вас высокий уровень дохода');
-     } else if (budgetDay > 600 && budgetDay <= 1200) {
-          return('У вас средний уровень дохода');
-     } else if (budgetDay > 0 && budgetDay <= 600) {
-          return('К сожалению у вас уровень дохода ниже среднего');
-     } else if (budgetDay < 0) { 
-          return('Что то пошло не так'); 
-     } else if (budgetDay === 0  ) {
-          return('У Вас доход 0 ');
-     }
-}
-console.log(getStatusIncome());

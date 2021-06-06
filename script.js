10c017d7a0cdd3a73654bce7d3ea6345677e5105
Vadim Semchanka <vadimsemch@gmail.com>
2021-06-06 19:14:25 +0200

    pravka

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-'use strict'
+ 'use strict'
 
 let isNumber = function(n) {
      return !isNaN(parseFloat(n)) && isFinite(n)
 };
 
-const numberX = Math.floor(100*Math.random())+1;
-console.log('Число бота: ' + numberX);
-const gess = +prompt('Угадай число от 1 до 100');
-console.log(gess); 
+let randomNumber = function () {
+     let whyNum;
+     let getRandomNumber = function () {
+          do {
+               whyNum = prompt('Угадай число от 1 до 100');
+          }
+               while (!isNumber(whyNum) || whyNum === '' || whyNum === null);
+     };
+     getRandomNumber();
+     console.log(whyNum);
 
-const getGessNumber = function() {
-     if (gess > numberX ) {
-          return ('Загаданное число меньше');
-              
-     } else if (gess < numberX) {
-          return ('Загаданное число больше');
-             
-     } else if (!isNumber(gess)) {
-          return ('Введи число!') ; 
+     const randomNumber = function() {
+          console.log('Число бота: ' + (Math.floor(100*Math.random())+1));
+     };
+    
+     const num = randomNumber();
+     
+     
+     const getGessNumber = function() {
+          if (whyNum > num ) {
+               return ('Загаданное число меньше');
+                   
+          } else if (whyNum < num) {
+               return ('Загаданное число больше');
+                  
+          } else if (!isNumber(num)) {
+               return ('Введи число!') ; 
+               
+          } else if (whyNum === null) {
+               return ('Игра окончена');
+          } else if (whyNum === num) {
+               return ('Поздравляю, Вы угадали!!!');
+          }
+     };
+     console.log(getGessNumber());
           
-     } else if (gess === null) {
-          return ('Игра окончена');
-     } else if (gess === numberX) {
-          return ('Поздравляю, Вы угадали!!!');
-     }
-};
-console.log(getGessNumber());
+     
+}
+randomNumber();
